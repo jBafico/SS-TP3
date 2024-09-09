47da6124f1cae9ad6ab9f325b06afdda6f1a477e
Juan Cruz Bafico <jbafico@itba.edu.ar>
2024-09-09 16:15:33 -0300

    chore: started main

--- Simulation/src/main/java/org/example/Main.java
+++ Simulation/src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.text.SimpleDateFormat;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Date;
 import java.util.Optional;
 
 public class Main {
@@ -7,7 +14,7 @@ public class Main {
         System.out.println("Starting simulation!");
 
         // todo replace with json parameters
-        int numberOfParticles = 10;
+        int numberOfParticles = 30;
         double wallRadius = 100;
         double particleRadius = 1;
         double obstacleRadius = 3;
@@ -16,6 +23,31 @@ public class Main {
         Optional<Double> obstacleMass = Optional.empty();
         int maxEvents = 500;
 
-        MDSimulation simulation = new MDSimulation(numberOfParticles, wallRadius, particleRadius, obstacleRadius, velocityModulus, particleMass, obstacleMass, maxEvents);
+        // Get the current timestamp
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+        // Generate output json file name that has current timestamp
+        String filenameWithTimestamp = "simulation_" + timeStamp + ".json";
+
+        try (FileWriter writer = initializeOutputJson(filenameWithTimestamp)){
+            MDSimulation simulation = new MDSimulation(numberOfParticles, wallRadius, particleRadius, obstacleRadius, velocityModulus, particleMass, obstacleMass, maxEvents);
+            simulation.start(writer);
+            writer.write("\n]\n}");
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+
+
+    }
+
+    //TODO add params
+    private static FileWriter initializeOutputJson(String outputJsonName) throws IOException {
+        FileWriter writer = new FileWriter("./files/%s".formatted(outputJsonName));
+        writer.write("{\n\"global_params\": ");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //gson.toJson(global_params, writer);
+        writer.write(",\n");
+        writer.write("\"simulations\": [\n");
+        return writer;
     }
 }
